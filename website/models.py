f3a7874d27bfa4e8dda635160fbc42651e17c0d9
Mohamed Aymen Mhiri <85041519+Fenyx-bot@users.noreply.github.com>
2024-08-23 18:30:54 +0200

    added models

--- website/models.py
+++ website/models.py
@@ -1,16 +1,23 @@
 from . import db
 from flask_login import UserMixin
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
-
+ 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     username = db.Column(db.String(150), unique=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    notes = db.relationship('Note')
+
+class Word(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word_german = db.Column(db.String(150), unique=True)
+    definition_german = db.Column(db.String(150))
+    definition_english = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+class UserData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    health = db.Column(db.Integer)
+    score = db.Column(db.Integer)
+    total_words = db.Column(db.Integer)
